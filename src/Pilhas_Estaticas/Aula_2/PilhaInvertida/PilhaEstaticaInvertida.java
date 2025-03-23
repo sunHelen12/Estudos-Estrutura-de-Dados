7f576d5965f4b94eaa4cac41177baf8e1a392e37
sunHelen12 <sunrise.developer12@gmail.com>
2025-03-23 19:47:22 -0300

    Alterações

---
+++ src/Pilhas_Estaticas/Aula_2/PilhaInvertida/PilhaEstaticaInvertida.java
@@ -0,0 +1,88 @@
+package Pilhas_Estaticas.Aula_2.PilhaInvertida;
+
+public class PilhaEstaticaInvertida implements Empilhavel {
+    // variaveis de instancia
+    private int ponteiroTopo; // indice
+    private Object[] dados; // array
+    // métodos
+    // construtores
+
+    public PilhaEstaticaInvertida(int tamanho) {
+        dados = new Object[tamanho];
+        ponteiroTopo = tamanho;
+    }
+
+    public PilhaEstaticaInvertida() {
+        this(10); // chamamos o outro construtor, quando precisarmos mudar
+                  // só mudamos o primeiro construtor evitando inconsistência
+    }
+
+    // métodos principais
+    @Override
+    public void empilhar(Object elemento) {
+        if (!estaCheia()) {
+            ponteiroTopo--;
+            dados[ponteiroTopo] = elemento;
+        } else {
+            System.err.println("Pilha Cheia!");
+        }
+    }
+
+    @Override
+    public void atualizar(Object elemento) {
+        if (!estaVazia()) {
+            dados[ponteiroTopo] = elemento;
+        } else {
+            System.err.println("Pilha Vazia!");
+        }
+    }
+
+    @Override
+    public Object desempilhar() {
+        Object dadoEspiado = null;
+        if (!estaVazia()) {
+            dadoEspiado = dados[ponteiroTopo];
+            ponteiroTopo++;
+        } else {
+            System.out.println("Pilha Vazia!");
+        }
+        return dadoEspiado;
+    }
+
+    @Override
+    public Object espiar() {
+        Object dadoEspiado = null;
+        if (!estaVazia()) {
+            dadoEspiado = dados[ponteiroTopo];
+        } else {
+            System.out.println("Pilha Vazia!");
+        }
+        return dadoEspiado;
+    }
+
+    // métodos auxiliares
+    @Override
+    public boolean estaCheia() {
+        return (ponteiroTopo == 0);
+
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return (ponteiroTopo == dados.length);
+    }
+
+    @Override
+    public String imprimir() {
+        String aux = "[";
+        for (int i = ponteiroTopo; i < dados.length; i++) { // vamos até o ponteiroTopo, porque lá deve estar o último
+            if (i == dados.length - 1) { // elemento implementado
+                aux += dados[i];
+            } else {
+                aux += dados[i] + ", ";
+            }
+        }
+        return aux + "]";
+    }
+
+}
---
+++ src/Pilhas_Estaticas/Revisando/Pilha_Dupla/EmpilhavelDupla.java
@@ -0,0 +1,20 @@
+package Pilhas_Estaticas.Revisando.Pilha_Dupla;
+
+public interface EmpilhavelDupla {
+
+    //métodos da pilha 1 - tradicional
+    void empilhar1(Object elemento);
+    Object desempilhar1();
+    Object topo1();
+    boolean estaCheia1();
+    boolean estaVazia1();
+    String imprimir1();
+
+    //métodos da pilha 2 - invertida
+    void empilhar2(Object elemento);
+    Object desempilhar2();
+    Object topo2();
+    boolean estaCheia2();
+    boolean estaVazia2();
+    String imprimir2();
+}
