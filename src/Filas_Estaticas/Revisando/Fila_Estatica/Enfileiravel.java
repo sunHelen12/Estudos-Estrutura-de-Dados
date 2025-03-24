6e1577e6eca522d8f1cc602b041963fb10b2f90e
sunHelen12 <sunrise.developer12@gmail.com>
2025-03-23 21:25:03 -0300

    Fila Estatica

---
+++ src/Filas_Estaticas/Revisando/Fila_Estatica/Enfileiravel.java
@@ -0,0 +1,11 @@
+package Filas_Estaticas.Revisando.Fila_Estatica;
+
+public interface Enfileiravel {
+    void enfileirar(Object elemento); //enfileirar elementos
+    Object desenfileirar(); //desenfileirar elementos e o retornar
+    Object espiar(); //verificar elemento que está na frente da fila
+
+    boolean estaCheia(); //verificar se a fila está cheia
+    boolean estaVazia(); //verificar se a fila está vazia
+    String imprimir(); //imprimir elementos da fila
+}
