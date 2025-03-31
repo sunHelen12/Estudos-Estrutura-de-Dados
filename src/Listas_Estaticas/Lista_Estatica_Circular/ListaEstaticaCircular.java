72b7310aa508b8a27e5937646b99b1a42597ba5d
sunHelen12 <sunrise.developer12@gmail.com>
2025-03-31 00:13:39 -0300

    Lista

---
+++ src/Listas_Estaticas/Lista_Estatica_Circular/ListaEstaticaCircular.java
@@ -0,0 +1,116 @@
+package Listas_Estaticas.Lista_Estatica_Circular;
+
+public class ListaEstaticaCircular implements Listavel {
+    //Variáveis de instância
+    private int ponteiroInicio, ponteiroFim, quantidade;
+    Object [] elementos;
+
+    //Constutores
+    public ListaEstaticaCircular(int tamanho) {
+        this.elementos = new Object[tamanho]; //inicializamos
+        this.ponteiroInicio = 0;
+        this.ponteiroFim = -1;
+        quantidade = 0;
+    }
+
+    public ListaEstaticaCircular() {
+        this(10);
+    }
+
+    @Override
+    public void anexar(Object elemento) {
+        if(!estaCheia()){
+            ponteiroFim ++; //ponteiroFim avança para a próxima posição para não sobrescrever o elemento atual
+            if(ponteiroFim == elementos.length){ //se o ponteiroFim chegar no final do vetor, ele volta para o início
+                ponteiroFim = 0; //se o ponteiroFim chegar no final do vetor, ele volta para o início
+            }
+            quantidade++; //quando há inserções aumentamos a quantidade de  inserções
+            elementos[ponteiroFim] = elemento; //adicionamos o elemento na posição do ponteiroFim
+        } else {
+            System.err.println("Fila cheia");
+        }
+    }
+
+    //Método para que o usuário possa inserir um elemento em uma posição específica
+    @Override
+    public void inserir(Object elemento, int posicao) {
+        if(!estaCheia()){
+            //verificar se a posição está dentro do limite do vetor
+            if(posicao >= 0 && posicao <= quantidade){
+                int pontoManipulacao = (ponteiroInicio + posicao) % elementos.length; //calcular a posição do elemento que será inserido
+            
+                for (int i = ponteiroFim + 1; i != pontoManipulacao; i--){
+                    int anterior = i - 1;
+                    
+                    if (i == elementos.length){
+                        i = 0; //se o i chegar no final do vetor, ele volta para o início
+                    }
+
+                    int atual = i;
+                    elementos[atual] = elementos[anterior]; //deslocar os elementos para a direita
+                }
+
+                elementos[pontoManipulacao] = elemento; //adicionamos o elemento na posição do ponteiroFim
+                ponteiroFim++; //ponteiroFim avança para a próxima posição para não sobrescrever o elemento atual
+                //para manter a lista circular, se o ponteiroFim chegar no final do vetor, ele volta para o início
+                if(ponteiroFim == elementos.length){ 
+                    ponteiroFim = 0; //se o ponteiroFim chegar no final do vetor, ele volta para o início
+                }
+                quantidade++; //quando há inserções aumentamos a quantidade de  inserções   
+            }else{
+                System.err.println("Posição inválida");
+            }
+        } else {
+            System.err.println("Lista cheia");
+        }
+    }
+
+    @Override
+    public void selecionar(int posicao) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionar'");
+    }
+
+    @Override
+    public Object[] selecionarTodos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarTodos'");
+    }
+
+    @Override
+    public void atualizar(int posicao, Object novoElemento) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void remover(int posicao) {
+        
+    }
+
+    @Override
+    public boolean estaCheia() {
+       return (quantidade == elementos.length);
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return (quantidade == 0);
+    }
+
+    @Override
+    public String imprimir() {
+        //Mesma configuração da fila circular
+        String resultado = "[";
+        for (int i = 0, ponteiroAux = ponteiroInicio; i < quantidade; i++, ponteiroAux++){
+            if(quantidade == - 1){
+                resultado += elementos[ponteiroAux % elementos.length];
+            } else {
+                resultado += elementos[ponteiroAux % elementos.length] + ", ";
+            }
+        }
+        return resultado + "]";
+    }
+
+
+}
