861893a83a89bed30985b20b0f1b0f6ca96e6758
sunHelen12 <sunrise.developer12@gmail.com>
2025-05-19 00:31:38 -0300

    Ok

---
+++ src/UnidadeII/Fila_Dinamica/Aula/test/FilaDinamicaTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FilaDinamicaTest {
+
+    public void testEnfileirarComDiferentesTipos(){
+        Enfileirar<Integer> fila = new FilaDinamica<Integer>(10);
+    }
+}
