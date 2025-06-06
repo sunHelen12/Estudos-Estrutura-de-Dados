211ce9187217b9bf8fed5210d19cf4049cfa0b2b
sunHelen12 <sunrise.developer12@gmail.com>
2025-06-06 18:05:53 -0300

    Documentando Class NodoDuplo

--- UnidadeII/Pilhas_Dinamicas/Pilha_Dinamica/src/NodoDuplo.java
+++ UnidadeII/Pilhas_Dinamicas/Pilha_Dinamica/src/NodoDuplo.java
@@ -1,41 +1,79 @@
-/**
- * Para que serve?
- * @author
- * @version
- * @since
- *
- */
+ /**
+  * Classe que representa um nó duplo utilizado em estruturas de dados dinâmicas,
+  * como listas duplamente encadeadas ou pilhas dinâmicas.
+  * Cada nó armazena um dado genérico e referências para o nó anterior e o próximo.
+  *
+  * @param <T> Tipo dos dados armazenados no nó.
+  * @author
+  * @version 1.0
+  * @since 2025-05-01
+  */
 
-public class NodoDuplo <T>{
-    private NodoDuplo <T> anterior;
-    private T dado;
-    private  NodoDuplo <T> proximo;
+ public class NodoDuplo <T> {
+     /** Referência para o nó anterior. */
+     private NodoDuplo <T> anterior;
+     /** Dado armazenado no nó. */
+     private T dado;
+     /** Referência para o próximo nó. */
+     private NodoDuplo <T> proximo;
 
-    //Construtor
-    public NodoDuplo(T dado) {
-        this.dado = dado;
-        this.anterior = null;
-        this.proximo = null;
-    }
+     /**
+      * Construtor que inicializa o nó com o dado informado.
+      * As referências anterior e próximo são inicializadas como null.
+      *
+      * @param dado Dado a ser armazenado no nó.
+      */
+     public NodoDuplo(T dado) {
+         this.dado = dado;
+         this.anterior = null;
+         this.proximo = null;
+     }
 
-    // Documentação
+     /**
+      * Retorna o nó anterior.
+      * @return Referência para o nó anterior.
+      */
+     public NodoDuplo<T> getAnterior() {
+         return anterior;
+     }
 
-    public NodoDuplo<T> getAnterior() {
-        return anterior;
-    }
-    public void setAnterior(NodoDuplo<T> anterior) {
-        this.anterior = anterior;
-    }
-    public T getDado() {
-        return dado;
-    }
-    public void setDado(T dado) {
-        this.dado = dado;
-    }
-    public NodoDuplo<T> getProximo() {
-        return proximo;
-    }
-    public void setProximo(NodoDuplo<T> proximo) {
-        this.proximo = proximo;
-    }
-}
+     /**
+      * Define o nó anterior.
+      * @param anterior Referência para o nó anterior.
+      */
+     public void setAnterior(NodoDuplo<T> anterior) {
+         this.anterior = anterior;
+     }
+
+     /**
+      * Retorna o dado armazenado no nó.
+      * @return Dado do tipo T.
+      */
+     public T getDado() {
+         return dado;
+     }
+
+     /**
+      * Define o dado armazenado no nó.
+      * @param dado Novo dado do tipo T.
+      */
+     public void setDado(T dado) {
+         this.dado = dado;
+     }
+
+     /**
+      * Retorna o próximo nó.
+      * @return Referência para o próximo nó.
+      */
+     public NodoDuplo<T> getProximo() {
+         return proximo;
+     }
+
+     /**
+      * Define o próximo nó.
+      * @param proximo Referência para o próximo nó.
+      */
+     public void setProximo(NodoDuplo<T> proximo) {
+         this.proximo = proximo;
+     }
+ }
